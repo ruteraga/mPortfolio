784d946c4f3511efc07634a2e736187507de42d4
ruteraga <ganzagabin1@gmail.com>
2025-07-04 16:10:51 +0200

    adding new models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, Project, ProjectImage
+from .models import Tag, Project, ProjectImage, Category, Comment, Post
 # Register your models here.
 class ProjectImageInline(admin.TabularInline):
     model=ProjectImage
@@ -19,7 +19,17 @@ class Projectdatas(admin.ModelAdmin):
     list_display=("name",)
     search_fields=("name",)
 
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+class PostAdmin(admin.ModelAdmin):
+    pass
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectImage)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
