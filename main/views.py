abee7a9b747bbafe1922b5c688b45363ae51d96c
ruteraga <ganzagabin1@gmail.com>
2025-07-05 00:45:23 +0200

    change to views

--- main/views.py
+++ main/views.py
@@ -11,12 +11,24 @@ def project(request):
     #images=ProjectImage.objects.all()
     return render(request, "project.html", {"projects":projects}) 
 
-def blog_category(request):
+
+
+def blog(request):
     posts=Post.objects.all().order_by("-created_on")
     context={
         "posts":posts,
     }
+    return render(request, "blog.html", context)
+
+def blog_category(request, category):
+    posts=Post.objects.filter(categories__name__contains=category).order_by("-created_on")
+
+    context={
+        "category":category,
+        "posts":posts,
+    }
     return render(request, "blog_category.html", context)
+
 
 def blog_detail(request, pk):
     post=Post.objects.get(pk=pk)
