895167866e9702a1d6c13c802a47eb7626d0a73d
ruteraga <ganzagabin1@gmail.com>
2025-07-08 21:11:47 +0200

    Blog Page initial commit

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns=[
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("project/",views.project, name="project")
+    path("project/",views.project, name="project"),
+    path("blog/", views.blog, name="blog"),
+    path("post/<int:pk>/",views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category")
     #path("home/", views.home, name="home"),
     #path("contact/", views.contact, name="contact"),
     #path("project/<int:id>/", views.project, name="project")
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Project, Tag, ProjectImage, Post, Comment
+from main.forms import CommentForm
 # Create your views here.
 
 def home(request):
@@ -32,10 +34,23 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post=Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method=="POST":
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+        
     comments=Comment.objects.filter(post=post)
     context={
         "post":post,
         "comments":comments,
+        "form":CommentForm(),
     }
 
     return render(request, "blog_detail.html", context)
